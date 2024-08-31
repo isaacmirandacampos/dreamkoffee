a9451767a7b928c5b6975a5bf45c7a7adaaab267
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-08-31 11:29:03 -0300

    add new test case in update expense usecase

--- internal/usescases/expense/update_expense.go
+++ internal/usescases/expense/update_expense.go
@@ -9,6 +9,9 @@ import (
 )
 
 func (c *expenseUseCase) UpdateExpense(ctx context.Context, id *int, input model.UpdateExpense) (*model.Expense, error) {
+	if !input.Value.IsPositive() {
+		return nil, utils.ErrorHandling(ctx, 400, "value_must_be_positive", "Value must be positive", input.Value)
+	}
 	_, err := c.repo.GetExpense(ctx, int32(*id))
 	if err != nil {
 		return nil, utils.ErrorHandling(ctx, 404, "expense_not_found", "Expense not found", err.Error())
@@ -19,7 +22,7 @@ func (c *expenseUseCase) UpdateExpense(ctx context.Context, id *int, input model
 		Value:       input.Value,
 	})
 	if err != nil {
-		return nil, utils.ErrorHandling(ctx, 400, "bad_request", "Could not update expense", err.Error())
+		return nil, utils.ErrorHandling(ctx, 500, "internal_server_error", "Could not update expense", err.Error())
 	}
 	expense := &model.Expense{
 		ID:          int(updated.ID),
---
+++ internal/usescases/expense/update_expense_test.go
@@ -0,0 +1,113 @@
+package expense_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/applications/graph/model"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/storage/persistence"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/test/helper"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/test/mocks"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/usescases/expense"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateExpense(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRepo := mocks.NewMockRepository(ctrl)
+	repository := helper.RepositoryMock(mockRepo)
+	useCase := expense.NewExpenseUseCase(repository)
+	ctx := context.Background()
+	id := 1
+
+	t.Run("Should update a expense", func(t *testing.T) {
+		value, err := utils.UnmarshalDecimal("5.0")
+		assert.NoError(t, err)
+		input := model.UpdateExpense{
+			Description: "Coffee",
+			Value:       value,
+		}
+		expectedExpense := persistence.Expense{
+			ID:          1,
+			Description: "Coffee",
+			Value:       value,
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+		}
+
+		mockRepo.EXPECT().GetExpense(ctx, int32(id)).Times(1).Return(persistence.Expense{}, nil)
+
+		mockRepo.EXPECT().UpdateExpense(ctx, persistence.UpdateExpenseParams{
+			Description: input.Description,
+			Value:       input.Value,
+			ID:          1,
+		}).Times(1).Return(expectedExpense, nil)
+		result, err := useCase.UpdateExpense(ctx, &id, input)
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, int(expectedExpense.ID), result.ID)
+		assert.Equal(t, expectedExpense.Description, result.Description)
+		assert.Equal(t, expectedExpense.Value, result.Value)
+		assert.Equal(t, expectedExpense.CreatedAt.String(), result.CreatedAt)
+		assert.Equal(t, expectedExpense.UpdatedAt.String(), result.UpdatedAt)
+	})
+
+	t.Run("Shouldn't accept update a expense with negative value", func(t *testing.T) {
+		value, err := utils.UnmarshalDecimal("-5.0")
+		assert.NoError(t, err)
+		input := model.UpdateExpense{
+			Description: "Coffee",
+			Value:       value,
+		}
+		_, err = useCase.UpdateExpense(ctx, &id, input)
+		assert.EqualError(t, err, "input: Value must be positive")
+	})
+
+	t.Run("Shouldn't found a existent expense", func(t *testing.T) {
+		value, err := utils.UnmarshalDecimal("5.0")
+		assert.NoError(t, err)
+		input := model.UpdateExpense{
+			Description: "Coffee",
+			Value:       value,
+		}
+		mockRepo.EXPECT().GetExpense(ctx, int32(id)).Times(1).Return(persistence.Expense{}, errors.New("Should not found expense"))
+		result, err := useCase.UpdateExpense(ctx, &id, input)
+		assert.Error(t, err)
+		if err == nil {
+			t.Fatalf("Was expected error")
+		}
+		if result != nil {
+			t.Fatalf("Was expected nil in result")
+		}
+		assert.Equal(t, "input: Expense not found", err.Error())
+	})
+
+	t.Run("Should fail update a expense", func(t *testing.T) {
+		value, err := utils.UnmarshalDecimal("5.0")
+		assert.NoError(t, err)
+		input := model.UpdateExpense{
+			Description: "Coffee",
+			Value:       value,
+		}
+		mockRepo.EXPECT().GetExpense(ctx, int32(id)).Times(1).Return(persistence.Expense{}, nil)
+
+		mockRepo.EXPECT().UpdateExpense(ctx, persistence.UpdateExpenseParams{
+			Description: input.Description,
+			Value:       input.Value,
+			ID:          1,
+		}).Times(1).Return(persistence.Expense{}, errors.New("Invalid input"))
+		result, err := useCase.UpdateExpense(ctx, &id, input)
+		if err == nil {
+			t.Fatalf("Was expected error")
+		}
+		if result != nil {
+			t.Fatalf("Was expected nil in result")
+		}
+		assert.Equal(t, "input: Could not update expense", err.Error())
+	})
+}
