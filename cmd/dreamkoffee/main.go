01d76e1931381b6f0c3050b88c9ffbde610a8a50
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-09-02 22:48:08 -0300

    refactor how initilize the mux server

--- cmd/dreamkoffee/main.go
+++ cmd/dreamkoffee/main.go
@@ -4,12 +4,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gorilla/mux"
-
 	"github.com/isaacmirandacampos/dreamkoffee/configs"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/applications"
-	"github.com/isaacmirandacampos/dreamkoffee/internal/applications/middleware"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/domain"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/infrastructure/database/postgres"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/infrastructure/database/postgres/persistence"
@@ -26,13 +22,7 @@ func main() {
 	defer connection.Close()
 	repo := persistence.New(connection)
 	repository := domain.NewRepository(repo)
-
-	router := mux.NewRouter()
-	router.Use(middleware.Auth, middleware.WithResponseWriter)
-
-	srv := applications.Initialize(&repository)
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	router.Handle("/query", srv)
+	router := applications.Initialize(&repository)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
 	log.Fatal(http.ListenAndServe(":"+defaultPort, router))
--- internal/applications/initialize.go
+++ internal/applications/initialize.go
@@ -2,13 +2,18 @@ package applications
 
 import (
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/mux"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/applications/directives"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/applications/handlers/graph"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/applications/handlers/graph/model"
+	"github.com/isaacmirandacampos/dreamkoffee/internal/applications/middleware"
 	"github.com/isaacmirandacampos/dreamkoffee/internal/domain"
 )
 
-func Initialize(repo *domain.Repository) (srv *handler.Server) {
+func Initialize(repo *domain.Repository) *mux.Router {
+	router := mux.NewRouter()
+	router.Use(middleware.Auth, middleware.WithResponseWriter)
 	c := graph.Config{Resolvers: &graph.Resolver{
 		Repo:     repo,
 		Expenses: []*model.Expense{},
@@ -19,7 +24,8 @@ func Initialize(repo *domain.Repository) (srv *handler.Server) {
 		},
 	}
 
-	srv = handler.NewDefaultServer(graph.NewExecutableSchema(c))
-
-	return
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(c))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+	return router
 }
